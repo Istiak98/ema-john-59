714423cdc3fa17fbcb2923b59fb534567823dcf0
Istiak98 <istiakahmed104127@gmail.com>
2021-10-10 18:01:39 +0600

    1update

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAO2Cs0xFnVk0FpHV-j9J7BHHq1p7mxjiQ",
+    authDomain: "ema-john-simple-d19b5.firebaseapp.com",
+    projectId: "ema-john-simple-d19b5",
+    storageBucket: "ema-john-simple-d19b5.appspot.com",
+    messagingSenderId: "185535832816",
+    appId: "1:185535832816:web:1eb12dfedc11e394b59835"
+  };
+
+  export default firebaseConfig;
